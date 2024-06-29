f75322ad436e57dfbdf10529ec4b6273e6bc11e7
jdpman <johndavidpinto@gmail.com>
2024-06-28 19:49:40 -0500

    lab 6

---
+++ setup.py
@@ -0,0 +1,14 @@
+#setup.py
+
+from setuptools import setup
+setup(
+   name='word_processors',
+   version='0.1',
+   authors = 'JD Pinto'
+   email = 'jp5ph@virginia.edu'
+   packages=['word_processors',],
+   license='MIT',
+   long_description=open('README.txt').read(),
+)
+
+
