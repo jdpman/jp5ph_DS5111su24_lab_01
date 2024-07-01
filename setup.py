fb6cb80fdf8c5a4b2bf93b1a18cc6234c840a814
jdpman <johndavidpinto@gmail.com>
2024-07-01 18:51:00 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pkg_jp5ph',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
